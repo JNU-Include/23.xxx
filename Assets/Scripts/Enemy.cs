bb845c8c732362e2e23d422bb4e805aa1b563008
Lming1 <spear131121@gmail.com>
2017-10-31 12:29:33 +0900

    ai

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+
+    UnityEngine.AI.NavMeshAgent pathfinder;
+    Transform target;
+
+    void Start()
+    {
+        pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(UpdatePath());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = .25f;
+
+        while (target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathfinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
