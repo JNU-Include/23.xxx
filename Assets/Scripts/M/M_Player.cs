3299971df954dc67de43035df49abb13601b2fd4
Lming1 <spear131121@gmail.com>
2017-11-01 22:11:43 +0900

    standard asset import

---
+++ Assets/Scripts/M/M_Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_Player : MonoBehaviour {
+	//Player Setting 
+	public int life;
+	Rigidbody rigdbody;
+
+	void Awake()
+	{
+		rigdbody = GetComponent<Rigidbody> ();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	// life controll
+	public int GetLife()
+	{
+		return life;
+	}
+	public void SetLife(int life) 
+	{
+		this.life = life;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void PlayerMove()
+	{
+		
+	}
+}
