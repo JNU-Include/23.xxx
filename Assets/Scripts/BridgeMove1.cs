6cd11e57d10dc3f62da59975bbc71fd823ca8d1f
Suye0n <incuriositas@naver.com>
2017-11-01 23:41:03 +0900

    숲1 1차수정본

--- Assets/Scripts/BridgeMove1.cs
+++ Assets/Scripts/BridgeMove1.cs
@@ -2,33 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BridgeMove1 : MonoBehaviour {
 
+public class BridgeMove1 : MonoBehaviour {
+  
     public float Speed = 10.0f;
 
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
-	
-	// Update is called once per frame
-	void Update () {
+  
+    // Update is called once per frame
+    void Update () {
 
-        Debug.Log(transform.eulerAngles.y);
-
+        
         transform.Rotate(0, Speed * Time.deltaTime, 0);
 
-        if (255.0f < transform.eulerAngles.y || 183 > transform.eulerAngles.y)
-            Speed *= -1;
+        /*if (0 <= transform.eulerAngles.y && transform.eulerAngles.y < 18)         
+            transform.Rotate(0, Speed * Time.deltaTime, 0);
+        
+        else if (321 < transform.eulerAngles.y && transform.eulerAngles.y <= 365)  
+            transform.Rotate(0, Speed * Time.deltaTime, 0);
+        
+        else       
+            Speed *= -1;*/
+            
         
 
-        /*if (183 < transform.eulerAngles.y && transform.eulerAngles.y < 255)
-            transform.Rotate(0, Speed * Time.deltaTime, 0);
-
-        else if (transform.eulerAngles.y < 183 || transform.eulerAngles.y > 255)
-            transform.Rotate(0, -Speed * Time.deltaTime, 0);*/
-
-
-
+        /*if (17 < transform.eulerAngles.y && transform.eulerAngles.y < 18)        
+            Speed *= -1;
+        
+        else if (320 > transform.eulerAngles.y && transform.eulerAngles.y > 319)                    
+            Speed *= -1; */     
     }
 }
--- Assets/Scripts/Jumper3.cs
+++ Assets/Scripts/Jumper3.cs
@@ -2,31 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jumper : MonoBehaviour
-{
-
+public class Jumper3 : MonoBehaviour {
     public float Power;
-
     // Use this for initialization
-
     void OnCollisionEnter(Collision collision)
     {
-        Vector3 Power = new Vector3(0, 450 * Time.deltaTime, 0);
-        Power = Power.normalized * 450;
+        Vector3 Power = new Vector3(0, 300 * Time.deltaTime, 0);
+        Power = Power.normalized * 300;
 
         collision.gameObject.GetComponent<Rigidbody>().AddForce(Power);
 
     }
 
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
-    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
