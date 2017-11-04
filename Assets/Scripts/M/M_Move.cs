4de52485d4bc55befb04157e169b747965cc3e00
Lming1 <spear131121@gmail.com>
2017-11-04 14:18:59 +0900

    충돌 fix

---
+++ Assets/Scripts/M/M_Move.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_Move : MonoBehaviour {
+	UnityEngine.AI.NavMeshAgent navAgent;
+	Transform movePoint;
+
+
+	void Awake () {
+
+		navAgent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		movePoint = GameObject.FindGameObjectWithTag ("MovePoint").transform;
+
+	}
+
+
+	void Update () {
+
+		Vector3 pos = Input.mousePosition;
+
+		//캐릭터가 목표지점에 도착하면 movePoint 를 비활성화한다.
+		if (navAgent.remainingDistance == 0.0f)
+		{
+			movePoint.gameObject.SetActive(false);
+		}
+
+
+		if (Input.GetMouseButtonDown (0))
+		{
+
+			Ray ray;
+
+
+			ray = Camera.main.ScreenPointToRay(pos);
+			RaycastHit hitInfo;
+
+
+
+			if(Physics.Raycast(ray, out hitInfo))
+			{
+
+				//hitInpo 좌표로 movePoint 좌표를 변경한다.
+				movePoint.position = hitInfo.point;
+				movePoint.gameObject.SetActive(true);
+
+				navAgent.SetDestination(hitInfo.point);
+			}
+		}
+	}
+
+
+
+}
--- Assets/Scripts/slow_effect.cs
+++ Assets/Scripts/slow_effect.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class slow_effect : MonoBehaviour {
 
     //원하는 지형에 넣으면 밟을때 감속효과 생김
-    //slowRate 로 감속률 조정
+    //trigger 걸린 오브젝트에 넣어도 감속효과 생김
     public bool slow = true;
     GameObject player;
-    public float slowRate = 0.3f;
+    public float slowRate = 0.3f; //slowRate 로 감속률 조정
     float originSpeed;
-    float loseSpeed;
-
+    float loseSpeed; // 감속으로 잃은 속력량
+    public float slowTime = 1f; //감속 지속시간
     
-    private void OnCollisionStay(Collision collision)
+    //감속 걸림
+    private void actSlow()
     {
         t_player_move player_move = GameObject.FindGameObjectWithTag("Player").GetComponent<t_player_move>();
         float originSpeed = player_move.Speed;
@@ -32,24 +33,52 @@ public class slow_effect : MonoBehaviour {
         }
     }
 
-    private void OnCollisionExit(Collision collision)
+    //감속 풀림
+    IEnumerator SlowPTime(float DelayTime)
     {
+        yield return new WaitForSeconds(DelayTime);
         t_player_move player_move = GameObject.FindGameObjectWithTag("Player").GetComponent<t_player_move>();
         float originSpeed = player_move.Speed;
         originSpeed = originSpeed + loseSpeed;
         player_move.getSpeed(originSpeed);
         slow = true;
+
     }
 
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnCollisionStay(Collision collision) // 밟을때 감속 관련 코드
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            actSlow();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(SlowPTime(slowTime));
+        }
+
+    }
+
+
+    private void OnTriggerStay(Collider other) //trigger체크된 뚫고 지나가는 투사체에 활용
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            actSlow();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(SlowPTime(slowTime));
+        }
+    }
+
 }
 
