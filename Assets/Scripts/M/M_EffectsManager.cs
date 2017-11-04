4336ae272134cd6e279ccb84c2e10658dd342a89
Lming1 <spear131121@gmail.com>
2017-11-04 23:29:07 +0900

    코루틴

---
+++ Assets/Scripts/M/M_EffectsManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_EffectsManager : MonoBehaviour {
+	public List<Transform> effects;
+	// Use this for initialization
+	public void StartEffect(string effectName)
+	{
+		for (int i = 0; i < effects.Count; i++) 
+		{
+			if (effects [i].name.CompareTo (effectName) == 0) 
+			{
+				effects [i].gameObject.SetActive (false);
+				effects [i].gameObject.SetActive (true);
+				break;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/M/M_StopEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_StopEffect : MonoBehaviour {
+
+	public float stopTime = 1.0f;
+
+	void OnEnable()
+	{
+		StartCoroutine (TimeStop ());
+	}
+
+	IEnumerator TimeStop()
+	{
+		yield return new WaitForSeconds (stopTime);
+		gameObject.SetActive (false);
+	}
+
+}
