cc9e6eb5b8957791bb92630d4c4d12482e1ae8c4
Lming1 <spear131121@gmail.com>
2017-11-04 18:08:01 +0900

    player controller

---
+++ Assets/Scripts/M/M_FSMBase.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class M_FSMBase : MonoBehaviour {
+	public CharacterController characterController;
+	public Animator anim;
+	public M_PlayerState PCState;
+	public bool isNewState;
+
+	protected virtual void Awake()
+	{
+		characterController = GetComponent<CharacterController> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	protected virtual void OnEnable() 
+	{
+		PCState = M_PlayerState.Idle;
+		StartCoroutine(FSMMain());
+	}
+
+	IEnumerator FSMMain()
+	{
+		while (true) {
+			isNewState = false;
+			yield return StartCoroutine (PCState.ToString ());
+		}
+	}
+
+	public void SetState(M_PlayerState newState)
+	{
+		isNewState = true;
+		PCState = newState;
+		anim.SetInteger ("state", (int)PCState);
+	}
+
+	protected virtual IEnumerator Idle()
+	{
+		do {
+			yield return null;
+		} while(!isNewState);
+	}
+}
---
+++ Assets/Scripts/M/M_FSMPlayer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_FSMPlayer : M_FSMBase {
+	public int currentHP = 100;
+	public int maxHP = 100;
+	public int exp = 0;
+	public int level = 1;
+	public int gold = 0;
+	public float attack = 10.0f;
+	public float attackRange = 1.2f;
+
+	protected override IEnumerator Idle()
+	{
+		do {
+			yield return null;
+		} while(!isNewState);
+	}
+
+	protected virtual IEnumerator Jump()
+	{
+		do {
+			yield return null;
+		} while(!isNewState);
+	}
+
+	protected virtual IEnumerator Attack()
+	{
+		do {
+			yield return null;
+		} while(!isNewState);
+	}
+
+	protected virtual IEnumerator Dead()
+	{
+		do {
+			yield return null;
+		} while(!isNewState);
+	}
+
+	protected virtual IEnumerator Skill1()
+	{
+		do {
+			yield return null;
+		} while(!isNewState);
+	}
+
+	protected virtual IEnumerator Run()
+	{
+		do {
+			yield return null;
+		} while(!isNewState);
+	}
+
+	protected virtual IEnumerator Hurt()
+	{
+		do {
+			yield return null;
+		} while(!isNewState);
+	}
+}
