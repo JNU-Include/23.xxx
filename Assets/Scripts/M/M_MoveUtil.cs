c6621047738e86af54ccb722af220e175d32c6b1
Lming1 <spear131121@gmail.com>
2017-11-04 20:29:28 +0900

    충돌 fix

---
+++ Assets/Scripts/M/M_MoveUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_MoveUtil : MonoBehaviour {
+	
+	public static float MoveFrame(CharacterController characterController, Transform target, float moveSpeed, float turnSpeed)
+	{
+		Transform t = characterController.transform;
+		Vector3 dir = target.position - t.position;
+		Vector3 dirXZ = new Vector3 (dir.x, 0f, dir.z);
+		Vector3 targetPos = t.position + dirXZ;
+		Vector3 framePos = Vector3.MoveTowards (t.position, targetPos, moveSpeed * Time.deltaTime);
+
+		characterController.Move (framePos - t.position + Physics.gravity);
+
+		RotateToDir (t, target, turnSpeed);
+
+		return Vector3.Distance (framePos, targetPos);
+	}
+
+	//get location, speed 
+	public static void RotateToDir()
+	{
+		
+	}
+
+}
