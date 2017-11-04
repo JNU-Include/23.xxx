4ce8c802b0d2f74dbaec89ac6ddc1e2b01112f0f
Lming1 <spear131121@gmail.com>
2017-11-04 16:32:14 +0900

    player state add

---
+++ Assets/Scripts/M/M_PlayerState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum M_PlayerState{
+	Idle = 0,
+	AttackRun = 1, 
+	Attack = 2,
+	Dead = 3,
+	Skill1 = 4,
+	Run = 5,
+}
