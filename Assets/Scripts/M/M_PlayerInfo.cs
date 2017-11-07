16e0cc9337857bf481a3c7fbe74a3d030321a9ce
Lming1 <spear131121@gmail.com>
2017-11-07 15:38:32 +0900

    fix

--- Assets/Scripts/M/M_PlayerInfo.cs
+++ Assets/Scripts/M/M_PlayerInfo.cs
@@ -16,11 +16,13 @@ public class M_PlayerInfo : MonoBehaviour {
 
 	void Awake()
 	{
-		mPlayer = GameObject.FindGameObjectWithTag ("Player").GetComponent<M_FSMPlayer> ();
+		mPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<M_FSMPlayer>();
 	}
 	// Update is called once per frame
 	void Update () {
-		level.text = mPlayer.level.ToString();
+//		level.text = mPlayer.level.ToString ();
+		Debug.Log(mPlayer.level.ToString());
+		level.text = "99";
 		hpBar.fillAmount = (float)mPlayer.currentHP / (float)mPlayer.maxHP;
 		mpBar.fillAmount = (float)mPlayer.currentMP / (float)mPlayer.maxMP;
 		expBar.fillAmount = ((float)mPlayer.exp % 100.0f) / 100.0f;
