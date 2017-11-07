0a16cdce38c7f25af047d0f8374cf9036cd9515f
Lming1 <spear131121@gmail.com>
2017-11-07 23:34:29 +0900

    fix

---
+++ Assets/Scenes/title/ExitButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitButton : MonoBehaviour {
+
+    public void Quit_Button()
+    {
+        Invoke("quitgame", .2f);
+    }
+
+    void quitgame()
+    {
+        Application.Quit();
+        
+    }
+}
