97b26d2927188cb80221bbdf55c2fc56260a414c
ojlee <dldhwn56@gmail.com>
2017-11-07 21:05:47 +0900

    1107

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,23 +4,69 @@ using System.Collections;
 [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
 public class Enemy : MonoBehaviour
 {
+    public static Enemy instance = null;
+    private void Awake()
+    {
+        instance = this;
+    }
+
 
     UnityEngine.AI.NavMeshAgent pathfinder;
-    Transform target;
+    Transform target = null;
+    Animator animator;
+
+    public float e_health;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+            //애니메이션 전환
+        }
+        else { }
+   
+    }
+
+    void e_trace()
+    {   
+        if (pathfinder.destination != transform.position)
+        {
+            StartCoroutine(UpdatePath());
+        }
+        else
+        {
+
+        }
+
+    }
+
+    void e_attack()
+    {
+
+    }
+
+    void e_hit()
+    {
+
+    }
+
+    void e_death()
+    {
+
+    }
+
+
+
 
     void Start()
     {
         pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        target = GameObject.FindGameObjectWithTag("Player").transform;
-
-        StartCoroutine(UpdatePath());
-    }
-
-    void Update()
-    {
-
     }
     
+
     IEnumerator UpdatePath()
     {
         float refreshRate = 0.1f;
