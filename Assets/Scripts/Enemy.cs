24007a94e4444b9e1c038aeb6d8d54914c4c1e1a
Lming1 <spear131121@gmail.com>
2017-11-08 22:19:48 +0900

    fix

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,48 +4,54 @@ using System.Collections;
 [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
 public class Enemy : MonoBehaviour
 {
+    Animator animator;
+    public float e_health;
+    public float e_distance = 1;
+    public float e_atkSpeed = 0.2f;
+    UnityEngine.AI.NavMeshAgent pathfinder;
+    Transform target;
+    GameObject player;
+    GameObject enemy;
+
     public static Enemy instance = null;
+
     private void Awake()
     {
         instance = this;
+        animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
     }
 
-
-    UnityEngine.AI.NavMeshAgent pathfinder;
-    Transform target = null;
-    Animator animator;
-
-    public float e_health;
-
-
-
-    private void OnTriggerEnter(Collider other)
+    /*
+    private void e_idle()
     {
-        if(other.gameObject.tag == "Player")
-        {
-            target = GameObject.FindGameObjectWithTag("Player").transform;
-            //애니메이션 전환
-        }
-        else { }
-   
+            if (e_distance > Vector3.Distance(enemy.transform.position, player.transform.position))
+            {
+                e_trace();
+            }
     }
+    */
 
     void e_trace()
     {   
         if (pathfinder.destination != transform.position)
         {
+            target = player.transform;
+            animator.SetBool("IsWalk", true);
             StartCoroutine(UpdatePath());
         }
-        else
-        {
 
-        }
 
     }
 
-    void e_attack()
+    IEnumerator e_attack()
     {
-
+        animator.SetBool("IsWalk", false);
+        animator.SetBool("IsAttack", true);
+        yield return new WaitForSeconds(e_atkSpeed);
+        animator.SetBool("IsAttack", false);
+        e_trace();
     }
 
     void e_hit()
@@ -64,19 +70,32 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        target = null;
+        e_trace();
+        
+        
+
+
+
     }
-    
+
 
     IEnumerator UpdatePath()
     {
-        float refreshRate = 0.1f;
+        float refreshRate = 0.15f;
 
         while (target != null)
         {
             Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
             pathfinder.SetDestination(targetPosition);
-            yield return new WaitForSeconds(refreshRate);
+            if (e_distance < Vector3.Distance(player.transform.position, enemy.transform.position) )
+            {
+                target = null;
+                break;
+            }
+                yield return new WaitForSeconds(refreshRate);
         }
+        StartCoroutine(e_attack());
     }
     
 }
